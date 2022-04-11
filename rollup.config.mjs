0457d4a49b4ef68f32bd4be5703f32b97ba8438e
Mitsunee <19474909+Mitsunee@users.noreply.github.com>
2022-04-11 15:17:16 +0200

    reconfigure tooling for typescript

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,6 +1,8 @@
 import { readFileSync, rmdirSync, existsSync } from "fs";
 import { join } from "path";
-import { makeRollupConfig } from "@foxkit/internal";
+import { builtinModules } from "module";
+import pluginTypescript from "rollup-plugin-typescript2";
+import typescript from "typescript";
 
 // clean dist dir
 const distPath = join(process.cwd(), "dist");
@@ -11,6 +13,53 @@ if (existsSync(distPath)) {
 const pkg = JSON.parse(
   readFileSync(join(process.cwd(), "package.json"), "utf8")
 );
-const config = makeRollupConfig(pkg);
+
+const external = [
+  ...builtinModules,
+  //...Object.keys(pkg.dependencies),
+  ...Object.keys(pkg.peerDependencies)
+];
+
+const plugins = [
+  pluginTypescript({
+    exclude: "node_modules/**",
+    typescript,
+    tsconfig: "tsconfig.json",
+    useTsconfigDeclarationsDir: true
+  })
+];
+
+function makeConfig({ input, key, ts = true }) {
+  const output = new Array();
+
+  if (pkg.exports[key].import) {
+    output.push({
+      file: pkg.exports[key].import,
+      format: "esm"
+    });
+  }
+
+  if (pkg.exports[key].require) {
+    output.push({
+      file: pkg.exports[key].require,
+      format: "cjs"
+    });
+  }
+
+  const config = { input, output, external };
+
+  if (ts) config.plugins = plugins;
+
+  return config;
+}
+
+const config = [
+  makeConfig({ input: "src/createApi.ts", key: "." }),
+  makeConfig({
+    input: "src/node-polyfill.js",
+    key: "./node-polyfill",
+    ts: false
+  })
+];
 
 export default config;
