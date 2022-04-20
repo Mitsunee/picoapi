59ae483c14620acfeb7571e4aa64de13a2d50986
Mitsunee <19474909+Mitsunee@users.noreply.github.com>
2022-04-20 14:39:42 +0200

    migrate to @foxkit/rollup-config

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,57 +1,5 @@
-import { readFileSync, rmdirSync, existsSync } from "fs";
-import { join } from "path";
-import { builtinModules } from "module";
-import pluginTypescript from "rollup-plugin-typescript2";
-import typescript from "typescript";
-
-// clean dist dir
-const distPath = join(process.cwd(), "dist");
-if (existsSync(distPath)) {
-  rmdirSync(join(process.cwd(), "dist"), { recursive: true, force: true });
-}
-
-const pkg = JSON.parse(
-  readFileSync(join(process.cwd(), "package.json"), "utf8")
-);
-
-const external = [
-  ...builtinModules,
-  //...Object.keys(pkg.dependencies),
-  ...Object.keys(pkg.peerDependencies)
-];
-
-const plugins = [
-  pluginTypescript({
-    exclude: "node_modules/**",
-    typescript,
-    tsconfig: "tsconfig.json",
-    useTsconfigDeclarationsDir: true
-  })
-];
-
-function makeConfig({ input, key, ts = true }) {
-  const output = new Array();
-
-  if (pkg.exports[key].import) {
-    output.push({
-      file: pkg.exports[key].import,
-      format: "esm"
-    });
-  }
-
-  if (pkg.exports[key].require) {
-    output.push({
-      file: pkg.exports[key].require,
-      format: "cjs"
-    });
-  }
-
-  const config = { input, output, external };
-
-  if (ts) config.plugins = plugins;
-
-  return config;
-}
+import initConfig from "@foxkit/rollup-config/ts.js";
+const makeConfig = initConfig();
 
 const config = [
   makeConfig({ input: "src/createApi.ts", key: "." }),
